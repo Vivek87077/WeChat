8f9d84fcafa21d85c9a8071676520f8ef3e96edc
Vivek Sharma <144247877+Vivek87077@users.noreply.github.com>
2025-10-03 00:33:11 +0530

    Update config.js

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: [
+    "https://chatklr.netlify.app",
     "http://localhost:5173",
-    "http://localhost:4173",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
